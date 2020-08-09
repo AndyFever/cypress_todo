9b89c3e190ba372f4d3cfb9b39d6544d80288f58
Andy Fever <andy.fever@gmail.com>
2020-08-09 18:06:24 +0100

    Added todo steps

--- cypress/integration/cucumber-tests/completedTodo/completedTodoSteps.js
+++ cypress/integration/cucumber-tests/completedTodo/completedTodoSteps.js
@@ -1,7 +1,8 @@
 import {Given, Then, When} from "cypress-cucumber-preprocessor/steps"
+import todoPage from "../todo/todoPage";
 
 Given('I have a task to do', () => {
-    cy.visit('http://127.0.0.1:8888/#/')
+    todoPage.visitTodo()
     cy.get('.new-todo').clear()
     cy.get('.new-todo').type("Buy some eggs! {enter}")
 })
@@ -14,6 +15,6 @@ When('I clear all completed task', () => {
     cy.get('.clear-completed').should('be.visible').click()
 })
 
-Then('I shouldn\'t have the task on the todo list', () => {
+Then('I shouldnt have the task on the todo list', () => {
     cy.get('.clear_completed').should('not.be.visible')
 })
